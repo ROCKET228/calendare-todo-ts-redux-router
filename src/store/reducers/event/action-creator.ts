4605f29cea70de322e4b230ad72251ef8935118a
ROCKET228 <michkahap@gmail.com>
2023-03-25 17:31:57 +0200

    End redusers for event

--- src/store/reducers/event/action-creator.ts
+++ src/store/reducers/event/action-creator.ts
@@ -1,9 +1,17 @@
 import {IUser} from "../../../models/IUser";
 import {IEvent} from "../../../models/IEvent";
 import {EventActionEnum, SetEventAction, SetGuestsAction} from "./types";
+import {AppDispatch} from "../../index";
+import axios from "axios";
 
 export const EventActionCreators = {
     setGuests: (payload: IUser[]):SetGuestsAction => ({type: EventActionEnum.SET_GUESTS, payload}),
     setEvent: (payload: IEvent[]):SetEventAction => ({type: EventActionEnum.SET_EVENTS, payload}),
-    fetchGuests:
+    fetchGuests: () => async (dispatch: AppDispatch) => {
+        try {
+            const gusests = await axios.get('./users.json')
+        }catch (e){
+            console.log(e);
+        }
+    }
 }
